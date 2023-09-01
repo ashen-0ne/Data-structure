796e743362311c7646f3d408c8b2f6e07a14fff8
zhaobowen <bowen.zhao@greenstonesoft.com>
2023-09-01 15:33:55 +0800

    fix rb_tree insert

--- inc/RB_tree.h
+++ inc/RB_tree.h
@@ -1,6 +1,8 @@
 #ifndef RB_TREE_H
 #define RB_TREE_H
 
+#include <map>
+
 enum COLOR
 {
     BLACK = 0,
@@ -38,24 +40,28 @@ public:
     RB_tree(RB_tree &) = default;
     RB_tree(RB_tree &&) = default;
 
-    bool insert(Key key,Value value);
-    bool rotate_R(RB_tree_node<Key,Value> *& node);
-    bool rotate_L(RB_tree_node<Key,Value> *& node);
+    void insert(Key key,Value value);
+    int erase(Key key);
+    void rotate_R(RB_tree_node<Key,Value> * node);
+    void rotate_L(RB_tree_node<Key,Value> * node);
     void print_node();
     void print_node_(RB_tree_node<Key,Value>* node);
 
-private:
+    std::map<int,int> get_all_route_black_node_count();
+    void recursive_count(const RB_tree_node<Key,Value> * node,int &index,int count,std::map<int,int> & m);
+
+public:
     RB_tree_node<Key,Value> * m_root_node{nullptr};
 };
 
 template<class Key,class Value>
-bool RB_tree<Key,Value>::insert(Key key,Value value)
+void RB_tree<Key,Value>::insert(Key key,Value value)
 {
     if(m_root_node == nullptr)
     {
         m_root_node = new RB_tree_node<Key,Value>(key,value);
         m_root_node->m_color = COLOR::BLACK;
-        return true;
+        return;
     }
 
     RB_tree_node<Key,Value> * parent = nullptr;
@@ -64,7 +70,8 @@ bool RB_tree<Key,Value>::insert(Key key,Value value)
     {
         if(node->m_key == key)
         {
-            return false;
+            node->m_value = value;
+            return;
         }
 
         if(node->m_key > key)
@@ -90,92 +97,79 @@ bool RB_tree<Key,Value>::insert(Key key,Value value)
         parent->m_right_node = node;
     }
 
-    //1.parent为黑，直接插入不用变色
-    if(parent->m_color == COLOR::BLACK)
+    while(node != m_root_node && node->m_parent->m_color == COLOR::RED)
     {
-        return true;
-    }
-
-    //2.parent为红，叔叔节点为红。parent和叔叔节点变为黑，爷爷节点变红。向上调整(相当于爷爷节点插入一个新节点)
-    RB_tree_node<Key,Value> * uncle = nullptr;
-    RB_tree_node<Key,Value> * grandpa = parent->m_parent;
-
-    if(grandpa->m_left_node == parent)
-    {
-        uncle = grandpa->m_right_node;
-    }
-    else
-    {
-        uncle = grandpa->m_left_node;
-    }
-
-    while(true)
-    {
-        if(uncle && uncle->m_color == COLOR::RED)
+        RB_tree_node<Key,Value> * xpp = node->m_parent->m_parent;
+        //父节点为祖父节点的左节点
+        if(node->m_parent == xpp->m_left_node)
         {
-            parent->m_color = COLOR::BLACK;
-            uncle->m_color = COLOR::BLACK;
-            grandpa->m_color = COLOR::RED;
+            RB_tree_node<Key,Value> * uncle = xpp->m_right_node;
+            //情况1，如果存在叔节点，并且叔节点颜色为红色
+            if(uncle && uncle->m_color == COLOR::RED)
+            {
+                //调整父节点和叔节点颜色为黑，祖父节点为红。祖父节点子树路径上黑色节点数量不变
+                //当前节点跳至祖父节点，开始下一轮调整
+                uncle->m_color = COLOR::BLACK;
+                node->m_parent->m_color = COLOR::BLACK;
+                xpp->m_color = COLOR::RED;
+                node = xpp;
+            }
+            //1.此节点为插入节点，此时叔节点不存在
+            //2.此节点为上一轮变色的祖父节点，此时叔节点为黑色
+            //此两种情况使用同样方法处理
+            else
+            {
+                //情况3，如果当前节点为父节点的右节点，对父节点进行一次左旋，调整为情况2
+                if(node == node->m_parent->m_right_node)
+                {
+                    node = node->m_parent;
+                    rotate_L(node);
+                }
+                //情况2，如果当前节点为父节点的左节点，父节点也是祖父节点的左节点，对祖父节点进行一次右旋
+                //原祖父节点为黑，现父节点替换祖父节点的位置，变为黑，无需继续向上调整
+                rotate_R(xpp);
+                node->m_parent->m_color = COLOR::BLACK;
+                xpp->m_color = COLOR::RED;
+            }
         }
+        //父节点为祖父节点的右节点
         else
         {
-            //1.parent和node都是左节点，右旋
-            if(grandpa->m_left_node == parent && parent->m_left_node == node)
+            RB_tree_node<Key,Value> * uncle = xpp->m_left_node;
+            //情况4.如果存在叔节点，并且叔节点颜色为红色
+            if(uncle && uncle->m_color == COLOR::RED)
             {
-                rotate_R(grandpa);
+                //调整父节点和叔节点颜色为黑，祖父节点为红。祖父节点子树路径上黑色节点数量不变
+                //当前节点跳至祖父节点，开始下一轮调整
+                uncle->m_color = COLOR::BLACK;
+                node->m_parent->m_color = COLOR::BLACK;
+                xpp->m_color = COLOR::RED;
+                node = xpp;
             }
-
-            //2.parent是左节点，node是右节点，先左旋再右旋
-            if(grandpa->m_left_node == parent && parent->m_right_node == node)
+            //1.此节点为插入节点，此时叔节点不存在
+            //2.此节点为上一轮变色的祖父节点，此时叔节点为黑色
+            else
             {
-                rotate_L(parent);
-                rotate_R(grandpa);
+                //情况6，如果当前节点为父节点的左节点，对父节点进行一次右旋，调整为情况5
+                if(node == node->m_parent->m_left_node)
+                {
+                    node = node->m_parent;
+                    rotate_R(node);
+                }
+                //情况5，如果当前节点为父节点的右节点，父节点也是祖父节点的右节点，对祖父节点进行一次左旋
+                //原祖父节点为黑，现父节点替换祖父节点的位置，变为黑，无需继续向上调整
+                rotate_L(xpp);
+                node->m_parent->m_color = COLOR::BLACK;
+                xpp->m_color = COLOR::RED;
             }
-
-            //3.parent和node都是右节点，左旋
-            if(grandpa->m_right_node == parent && parent->m_right_node == node)
-            {
-                rotate_L(grandpa);
-            }
-
-            //4.parent是右节点，node是左节点，先右旋再左旋
-            if(grandpa->m_right_node == parent && parent->m_left_node == node)
-            {
-                rotate_R(parent);
-                rotate_L(grandpa);
-            }
-        }
-
-        if(grandpa == m_root_node)
-        {
-            grandpa->m_color = COLOR::BLACK;
-            break;
-        }
-
-        node = grandpa;
-        parent = grandpa->m_parent;
-
-        if(parent == m_root_node)
-        {
-            return true;
-        }
-
-        grandpa = parent->m_parent;
-
-        if(grandpa->m_left_node == parent)
-        {
-            uncle = grandpa->m_right_node;
-        }
-        else
-        {
-            uncle = grandpa->m_left_node;
         }
     }
-
+    m_root_node->m_color = COLOR::BLACK;
 }
 
+
 template<class Key,class Value>
-bool RB_tree<Key,Value>::rotate_R(RB_tree_node<Key,Value> *& node)
+void RB_tree<Key,Value>::rotate_R(RB_tree_node<Key,Value> * node)
 {
     RB_tree_node<Key,Value> * parent = node->m_parent;
     RB_tree_node<Key,Value> * sub_L = node->m_left_node;
@@ -211,7 +205,7 @@ bool RB_tree<Key,Value>::rotate_R(RB_tree_node<Key,Value> *& node)
 }
 
 template<class Key,class Value>
-bool RB_tree<Key,Value>::rotate_L(RB_tree_node<Key,Value> *& node)
+void RB_tree<Key,Value>::rotate_L(RB_tree_node<Key,Value> * node)
 {
     RB_tree_node<Key,Value> * parent = node->m_parent;
     RB_tree_node<Key,Value> * sub_R = node->m_right_node;
@@ -265,5 +259,161 @@ void RB_tree<Key,Value>::print_node_(RB_tree_node<Key,Value>* node)
     print_node_(node->m_right_node);
 }
 
+template<class Key,class Value>
+std::map<int,int> RB_tree<Key,Value>::get_all_route_black_node_count()
+{
+    std::map<int,int> m;
+    int index = 0;
+    recursive_count(m_root_node,index,0,m);
+    return m;
+}
 
+template<class Key,class Value>
+void RB_tree<Key,Value>::recursive_count(const RB_tree_node<Key,Value> * node,int& index,int count,std::map<int,int> & m)
+{
+    if(node == nullptr)
+    {
+        m[index] = count + 1;
+        return;
+    }
+
+    if(node->m_color == COLOR::BLACK)
+    {
+        count++;
+    }
+
+    recursive_count(node->m_left_node,index,count,m);
+    index++;
+    recursive_count(node->m_right_node,index,count,m);
+    return;
+}
+
+template<class Key,class Value>
+int RB_tree<Key,Value>::erase(Key key)
+{
+    RB_tree_node<Key,Value> * node = m_root_node;
+    RB_tree_node<Key,Value> * parent = nullptr;
+
+    while(node)
+    {
+        if(node->m_key == key)
+        {
+            break;
+        }
+        else if(node->m_key > key)
+        {
+            parent = node;
+            node = node->m_left_node;
+        }
+        else
+        {
+            parent = node;
+            node = node->m_right_node;
+        }
+    }
+
+    if(node == nullptr)
+    {
+        return 0;
+    }
+
+    RB_tree_node<Key,Value> * sub_L = node->m_left_node;
+    RB_tree_node<Key,Value> * sub_LP = nullptr;
+
+    while(sub_L)
+    {
+        if(sub_L->m_right_node)
+        {
+            sub_LP = sub_L;
+            sub_L = sub_L->m_right_node;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    //节点没有左子树，右节点代替节点。包括只有root节点的情况
+    if(sub_L == nullptr)
+    {
+        RB_tree_node<Key,Value> * sub_R = node->m_right_node;
+        if(parent == nullptr)
+        {
+            m_root_node = sub_R;
+            m_root_node->m_color = COLOR::BLACK;
+        }
+        else if(parent->m_left_node == node)
+        {
+            parent->m_left_node = sub_R;
+        }
+        else
+        {
+            parent->m_right_node = sub_R;
+        }
+        if(sub_R)
+        {
+            sub_R->m_parent_node = parent;
+        }
+        delete node;
+        return 1;
+    }
+
+    //节点的左子树没有右节点，左节点代替节点
+    if(sub_LP == nullptr)
+    {
+        RB_tree_node<Key,Value> * sub_R = node->m_right_node;
+        if(parent == nullptr)
+        {
+            m_root_node = sub_L;
+        }
+        else if(parent->m_left_node == node)
+        {
+            parent->m_left_node = sub_L;
+        }
+        else
+        {
+            parent->m_right_node = sub_L;
+        }
+
+        sub_L->m_parent_node = parent;
+        sub_L->m_right_node = sub_R;
+
+        if(sub_R)
+        {
+            sub_R->m_parent_node = sub_L;
+        }
+
+        delete node;
+        return 1;
+    }
+
+    if(parent == nullptr)
+    {
+        m_root_node = sub_L;
+    }
+    else if(parent->m_left_node == node)
+    {
+        parent->m_left_node = sub_L;
+    }
+    else
+    {
+        parent->m_right_node = sub_L;
+    }
+
+    sub_LP->m_right_node = sub_L->m_left_node;
+    
+    if(sub_LP->m_right_node == nullptr)
+    {
+        sub_LP->m_bf -= 1;
+    }
+    
+    sub_L->m_parent_node = parent;
+    sub_L->m_right_node = node->m_right_node;
+    sub_L->m_left_node = node->m_left_node;
+
+    
+
+    delete node;
+    return 1;
+}
 #endif
--- main.cpp
+++ main.cpp
@@ -5,70 +5,69 @@
 int main(int argc,char ** argv)
 {
     srand(time(0));
-    AVL_tree<int,std::string> tree;
+    // AVL_tree<int,std::string> tree;
 
     // for(int i = 20;i < 10000;i += 10)
     // {
     //     tree.insert(rand()%1000000,std::to_string(rand()%1000000));
     // }
 
-    // for(int i = 20;i < 10000;i += 10)
+    // for(int i = 20;i < 1000;i += 1)
     // {
-    //     tree.erase(rand()%1000000);
+    //     tree.insert(i,std::to_string(i));
+    // }
+    
+    // for(int i = 50;i < 500;i += 3)
+    // {
+    //     tree.erase(i);
     // }
 
-    // for(int i = 20;i < 10000;i += 10)
+    // tree.print_node();
+
+    // std::map<int,int> m;
+
+    // for(int i = 0;i < 10000;++i)
     // {
-    //     tree.insert(rand()%1000000,std::to_string(rand()%1000000));
+    //     m[i] = i;
     // }
 
-    tree.print_node();
+    // m.insert(m.begin(),std::make_pair<int,int>(114514,114514));
 
-    // RB_tree<int,int> rb_tree;
-    // for(int i = 20;i < 10000;i += 10)
+    // std::cout<<m.begin()->second<<std::endl;
+
+    // for(auto i = m.begin();i != m.end();++i)
     // {
-    //     rb_tree.insert(rand()%1000000,rand()%1000000);
+    //     std::cout<<i->second<<std::endl;
     // }
 
-    // for(int i = 300; i < 600; i+= 10)
+    // std::cout<<m.begin()->second<<std::endl;
+
+    RB_tree<int,int> rb_tree;
+    for(int i = 20;i < 10000;i += 10)
+    {
+        rb_tree.insert(rand()%1000000,rand()%1000000);
+    }
+
+    // for(int i = 1;i < 10000;++i)
     // {
     //     rb_tree.insert(i,i);
     // }
 
-    // for(int i = 300,i < 600;i += 10)
+    // for(int i = 10000;i > 0;--i)
     // {
-    //     rb_tree.erase(i);
+    //     rb_tree.insert(i,i);
     // }
 
-    // rb_tree.print_node();
-    // for(int i = 50000;i >= 10000;i -= 10)
-    // {
-    //     tree.insert(i,std::to_string(i));
-    //     // tree.level_print();
-    // }
+    // rb_tree.insert(10,10);
 
-    // tree.insert(20,"20");
-    // tree.insert(30,"30");
-    // tree.insert(25,"25");
+    std::cout<<"m_root_node->m_key:"<<rb_tree.m_root_node->m_key<<std::endl;
 
-    // tree.insert(30,"30");
-    // tree.insert(20,"20");
-    // tree.insert(40,"40");
-    // tree.insert(35,"35");
-    // tree.insert(50,"50");
-    // tree.insert(34,"34");
-    // tree.insert(36,"36");
+    std::map<int,int> m = rb_tree.get_all_route_black_node_count();
 
-    // tree.insert(10,"10");
-    // tree.insert(20,"20");
-    // tree.print_node();
-    // tree.insert(30,"30");
-    // tree.print_node();
-    // tree.insert(40,"40");
-    // tree.print_node();
-    // tree.insert(50,"50");
-    // tree.print_node();
-    // tree.insert(60,"60");
+    for(auto i = m.begin();i != m.end(); ++i)
+    {
+        std::cout<<"index:"<<i->first<<" count:"<<i->second<<std::endl;
+    }
 
     return 0;
 }
